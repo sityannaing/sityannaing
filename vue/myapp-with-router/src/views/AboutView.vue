2eebc20a2fdbb1593e196a655f10cec51355bd7c
Tutorials-Git <sityannaing2609@gmail.com>
2022-10-03 21:50:06 +0630

    added toml file

---
+++ vue/myapp-with-router/src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <Header title="About Page" subtitle="Say Something!" />
+  <div class="about">
+    <h1>About Page</h1>
+    
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue';
+  export default{
+    name: "AboutView",
+    components: { Header }
+}
+</script>
